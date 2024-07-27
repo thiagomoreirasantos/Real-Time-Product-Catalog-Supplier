0085a5b7a393121c3a2e007778d84ebe63ed2ac4
thiagomoreirasantos <tmoreiradossantos733@gmail.com>
2024-07-27 20:57:51 +0100

    Refactoring

--- src/Real-Time-Product-Catalog-Consumer/Program.cs
+++ src/Real-Time-Product-Catalog-Consumer/Program.cs
@@ -1,23 +1,34 @@
 using Real_Time_Product_Catalog_Consumer;
 using Real_Time_Product_Catalog_Consumer.Configuration;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Consumer>();
-
-builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-builder.Configuration.AddEnvironmentVariables();
-
-builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
-
-builder.Services.PostConfigure<AppSettings>(options =>
-{
-    builder.Services.AddHttpClient(options.Url, config =>
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
     {
-        config.BaseAddress = new Uri(options.Url);
-        config.Timeout = TimeSpan.FromMinutes(5);
+        var env = context.HostingEnvironment;
+
+        config.SetBasePath(Directory.GetCurrentDirectory());
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+        config.AddEnvironmentVariables();
+
+        if (args != null)
+        {
+            config.AddCommandLine(args);
+        }
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.Configure<AppSettings>(hostContext.Configuration.GetSection("AppSettings"));
+        services.PostConfigure<AppSettings>(options =>
+        {
+            services.AddHttpClient(options.Url, config =>
+            {
+                config.BaseAddress = new Uri(options.Url);
+                config.Timeout = TimeSpan.FromMinutes(5);
+            });
+        });
+        services.AddHostedService<Consumer>();
     });
-});
 
 var host = builder.Build();
 await host.RunAsync();
